1cbbcf0e38f62c6419ed6803ba32bcb800dbf874
student <student@dons-imac.local>
2016-03-22 16:38:32 -0400

    Created MyContacts Table View, and modeled core data. Core Data may need revision.

---
+++ MyContacts/Contact+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  Contact+CoreDataProperties.swift
+//  MyContacts
+//
+//  Created by student on 3/22/16.
+//  Copyright © 2016 Josh. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Contact {
+
+    @NSManaged var name: String
+    @NSManaged var email: String
+    @NSManaged var phone: String
+
+}
---
+++ MyContacts/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  MyContacts
+//
+//  Created by student on 3/22/16.
+//  Copyright © 2016 Josh. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Contact: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
