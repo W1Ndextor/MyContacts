7ca61697546a2474cc4055cbaf04635218aaed56
student <student@mh215-16s-imac.local>
2016-04-12 15:52:20 -0400

    tinkering with color

--- MyContacts/MyContactsTableViewController.swift
+++ MyContacts/MyContactsTableViewController.swift
@@ -59,12 +59,50 @@ class MyContactsTableViewController: UITableViewController {
             fatalError("There was an error fetching contacts!")
         }
     }
+    //////////////////
+    func colorWithHexString (hex:String) -> UIColor {
+        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
+        
+        if (cString.hasPrefix("#")) {
+            cString = (cString as NSString).substringFromIndex(1)
+        }
+        
+        if (cString.characters.count != 6) {
+            return UIColor.grayColor()
+        }
+        
+        let rString = (cString as NSString).substringToIndex(2)
+        let gString = ((cString as NSString).substringFromIndex(2) as NSString).substringToIndex(2)
+        let bString = ((cString as NSString).substringFromIndex(4) as NSString).substringToIndex(2)
+        
+        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
+        NSScanner(string: rString).scanHexInt(&r)
+        NSScanner(string: gString).scanHexInt(&g)
+        NSScanner(string: bString).scanHexInt(&b)
+        
+        
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
+    }
+    //////////////////////////////
     
     //implementing addContact method
     func addContact(sender: AnyObject?) {
         
         //create instance of alertController
         let alert = UIAlertController(title: "Add", message: "Contact", preferredStyle: .Alert)
+        
+        
+        ////////////////////
+        let color1 = colorWithHexString("F52887")
+        let color2 = colorWithHexString("ff0000")
+        alert.view.backgroundColor = color1
+        alert.view.tintColor = color2
+        alert.view
+        alert.view.layer.cornerRadius = 36
+        //alert.view.layer
+        /////////////////////////
+        //alert.view.backgroundColor = UIColor.brownColor()
+        
         
         //define what happenes when the alert add button is pushed
         let addAction = UIAlertAction(title: "Add", style: .Default) { (action) -> Void in
